070677e8421c8d9811d5d5558e852512cfde9bbf
Monika Spielman <firelf@lothlorien.local>
2017-09-30 22:32:03 -0700

    appended snippets

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -21,4 +21,52 @@ class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext
   public function __construct() {
   }
 
+
+    /**
+     * @Then I should see the :arg1 button in the :arg2
+     */
+    public function iShouldSeeTheButtonInThe($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should see the :arg1 element in the :arg2
+     */
+    public function iShouldSeeTheElementInThe($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should not see the :arg1 element in the :arg2
+     */
+    public function iShouldNotSeeTheElementInThe($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should not see :arg1 in the :arg2 element in the :arg3
+     */
+    public function iShouldNotSeeInTheElementInThe($arg1, $arg2, $arg3)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should see the :arg1 element with the :arg2 attribute set to :arg3 in the :arg4 region
+     */
+    public function iShouldSeeTheElementWithTheAttributeSetToInTheRegion($arg1, $arg2, $arg3, $arg4)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should see :arg1 in the :arg2 element with the :arg3 attribute set
+     */
+    public function iShouldSeeInTheElementWithTheAttributeSet($arg1, $arg2, $arg3)
+    {
+        throw new PendingException();
+    }
 }
